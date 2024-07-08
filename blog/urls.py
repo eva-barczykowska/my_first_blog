9691afdc050996aea6061a2455c6c1d9fc6562c6
USER <user@USERs-MacBook-Pro.local>
2024-07-08 16:26:49 +0200

    fix url

--- blog/urls.py
+++ blog/urls.py
@@ -4,15 +4,3 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
-
-# """mysite URL Configuration
-
-# [...]
-# """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
